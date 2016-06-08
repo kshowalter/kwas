fac1ddb016391c8a6f505ef38926948a7cf8a58a
Keith Showalter <kshowalter@fsec.ucf.edu>
2016-06-08 09:59:45 -0400

    Tweak to default webpage

--- client/app.js
+++ client/app.js
@@ -1,9 +1,11 @@
+console.log('script loaded');
+
 window.onload = function(){
   console.log('page loaded');
 
   var notice = document.createElement('span');
   notice.className = 'notice';
-  notice.innerHTML = 'ES file loaded';
+  notice.innerHTML = 'app loaded';
   var appElement = document.getElementById('content');
   appElement.appendChild(notice);
 
