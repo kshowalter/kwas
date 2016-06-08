87ec302ee972b1b88c91e256969a01c52545534d
Keith Showalter <kshowalter@fsec.ucf.edu>
2016-06-08 09:58:54 -0400

    fixed webpack entry name

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ var path = require('path');
 
 module.exports = {
   entry: {
-    index: ['./client/app.js']
+    app: ['./client/app.js']
   },
   output: {
     path: path.resolve(__dirname, 'public'),
