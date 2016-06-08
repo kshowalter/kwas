555f873021314f8f7fcd708ca27537507be5a7fb
Keith Showalter <kshowalter@fsec.ucf.edu>
2016-06-08 09:47:16 -0400

    Misc updates, switch to webpack-dev-server

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # KWAS
+
 Basic starter with:
-* Express serving a public folder
 * Webpack building the client app
 * ESlint config file
-* other misc files
--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,31 @@
 var webpack = require('webpack');
+//var HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require('path');
 
 module.exports = {
   entry: {
-    app: './client/app.js'
+    index: ['./client/app.js']
   },
   output: {
-    path: './public/',
+    path: path.resolve(__dirname, 'public'),
+    publicPath: '/',
     filename: '[name].js'
-    //devtoolLineToLine: true,
   },
-  //devtool: 'eval',
-  //devtool: 'eval-source-map',
 
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('dev')
-    })
-  ],
   module: {
     loaders: [
       {
-        //test: /\.es6$/,
         test: /\.js$/,
-        //test: path.join(__dirname, 'src_browser'),
-        exclude: /(node_modules|bower_components)/,
         //exclude: /node_modules/,
+        include: /client/,
         loader: 'babel-loader',
         query: {
           // https://github.com/babel/babel-loader#options
           cacheDirectory: true,
           presets: ['es2015']
         }
-      },
-      {
-        test: /\.md$/,
-        loader: 'html!markdown'
       }
     ]
-
   }
+
 };
